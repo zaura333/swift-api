3bc5c7739c812dbd90486dc5c0e1890751fd0cb4
zaura333 <laura.wronskaa@gmail.com>
2025-02-13 22:34:15 +0100

    Initial index.ts for app config

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Express } from 'express';
+import routes from './routes/routes.js';
+import { connect } from './utils/db.js';
+
+dotenv.config();
+connect();
+
+const app: Express = express();
+const port = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
