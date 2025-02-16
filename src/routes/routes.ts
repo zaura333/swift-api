30ee96cbf618568de69664a11bea580118cd4e5c
zaura333 <laura.wronskaa@gmail.com>
2025-02-16 23:42:42 +0100

    Add health endpoint to run tests in docker

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import * as swiftController from '../controllers/swiftController';
+import { checkHealth } from '../controllers/healthController';
 
 const router = express.Router();
 
+router.get('/health', checkHealth);
 router.get('/swift-codes/:param', swiftController.routeSwiftOrIso2);
 router.post('/swift-codes', swiftController.postCode);
 router.delete('/swift-codes/:code', swiftController.deleteCode);
